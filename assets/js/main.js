ef6d774ceac342003d38648505aaf41a39e3ef44
dmitry <dimonshestak160201@gmail.com>
2021-07-25 19:38:15 +0300

    backup

---
+++ assets/js/main.js
@@ -0,0 +1,151 @@
+document.addEventListener('DOMContentLoaded', event => {
+
+
+const $IMG = document.getElementById('plugin-img').getBoundingClientRect()
+const WIDTH = $IMG.width
+const HEIGHT = $IMG.height
+const DPI_WIDTH = WIDTH * 2
+const DPI_HEIGHT = HEIGHT * 2
+
+
+function chart( canvas, data ){
+    const ctx = canvas.getContext('2d')
+    canvas.style.width = WIDTH + 'px'
+    canvas.style.height = HEIGHT + 'px'
+    canvas.width = DPI_WIDTH
+    canvas.height = DPI_HEIGHT
+
+    let renderPoints = []
+
+    function render(){
+        ctx.beginPath()
+        ctx.lineWidth = 4
+        ctx.strokeStyle = "red"
+        for(const [x, y] of renderPoints){
+            ctx.lineTo(x, y)
+        }
+        ctx.stroke()
+        ctx.closePath()
+
+        render_from_LC()
+    }
+
+    function render_from_LC(){
+        let all_path = []
+        if( localStorage.getItem('path') ){
+            all_path = JSON.parse( localStorage.getItem('path') ) 
+        }
+
+        all_path.forEach( item => {
+            ctx.beginPath()
+            ctx.lineWidth = 4
+            ctx.strokeStyle = "red"
+            for(const [x, y] of item.path){
+                ctx.lineTo(x, y)
+            }
+            ctx.stroke()
+            ctx.closePath()
+        } )
+        
+    }
+
+    function click(event){
+        let x = event.clientX
+        let y = event.clientY
+
+        renderPoints.push([x * 2, y * 2])
+        
+        render()
+    }
+    canvas.addEventListener('click', click)
+
+
+    let new_path = document.getElementById('new-path')
+    let finish_path = document.getElementById('finish-path')
+    let save_path = document.getElementById('save-path')
+
+    new_path.addEventListener('click', new_path_func)
+    finish_path.addEventListener('click', finish_path_func)
+    save_path.addEventListener('click', save_path_func)
+
+    function new_path_func(event){
+        canvas.addEventListener('click', click)
+    }
+    function finish_path_func(event){
+        let x = renderPoints[0][0]
+        let y = renderPoints[0][1]
+
+        renderPoints.push([x, y])
+
+        render()
+        save_LC()
+        canvas.removeEventListener('click', click)
+        renderPoints = []
+    }
+    function save_path_func(event){
+        console.log('save')
+    }
+
+
+    function save_LC(){
+        let all_objects = []
+        if( localStorage.getItem('path') ){
+            all_objects = JSON.parse( localStorage.getItem('path') ) 
+        }
+        
+        let product = {
+            id: '1',
+            color: 'red',
+            path: renderPoints,
+            points: devider(renderPoints)
+        }
+
+        all_objects.push( product )
+
+        localStorage.removeItem( 'path' )
+        //localStorage.setItem( 'path', JSON.stringify( all_objects ) )
+    }
+
+
+    function devider(rendPoints){
+        let x = rendPoints.map( item => item[0] )
+
+        let minX = Math.min( ...x )
+        let maxX = Math.max( ...x )
+
+        let points_array = []
+
+        for(let x = minX; x < maxX; x++){
+            renderPoints.forEach( (point, index) => {
+                console.log( intersection( x, [point, rendPoints[index+1]] ) )
+                // if( intersection( x, [point, rendPoints[index+1]] ) != [] ){
+                //     points_array.push( intersection( x, [point, rendPoints[index+1]] ) )
+                // }
+            } )
+        }
+
+        return null
+    }
+
+
+    function intersection( x, line ){
+        let k = ( line[1][1] - line[0][1] ) / ( line[1][0] - line[0][0] )
+        let b = line[0][1] - k * line[0][0]
+
+        if( x >= line[0][0] && x <= line[1][0] ){
+            return  [x, k*x+b]
+        }else{
+            return false
+        }
+    }
+
+
+    render()
+
+}
+
+
+chart( document.getElementById('plugin-canvas'), [] )
+
+
+} )
---
+++ dist/main.cea5deef.js
@@ -0,0 +1,513 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"assets/js/main.js":[function(require,module,exports) {
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  var $IMG = document.getElementById('plugin-img').getBoundingClientRect();
+  var WIDTH = $IMG.width;
+  var HEIGHT = $IMG.height;
+  var DPI_WIDTH = WIDTH * 2;
+  var DPI_HEIGHT = HEIGHT * 2;
+
+  function chart(canvas, data) {
+    var ctx = canvas.getContext('2d');
+    canvas.style.width = WIDTH + 'px';
+    canvas.style.height = HEIGHT + 'px';
+    canvas.width = DPI_WIDTH;
+    canvas.height = DPI_HEIGHT;
+    var renderPoints = [];
+
+    function render() {
+      ctx.beginPath();
+      ctx.lineWidth = 4;
+      ctx.strokeStyle = "red";
+
+      var _iterator = _createForOfIteratorHelper(renderPoints),
+          _step;
+
+      try {
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
+          var _step$value = _slicedToArray(_step.value, 2),
+              x = _step$value[0],
+              y = _step$value[1];
+
+          ctx.lineTo(x, y);
+        }
+      } catch (err) {
+        _iterator.e(err);
+      } finally {
+        _iterator.f();
+      }
+
+      ctx.stroke();
+      ctx.closePath();
+      render_from_LC();
+    }
+
+    function render_from_LC() {
+      var all_path = [];
+
+      if (localStorage.getItem('path')) {
+        all_path = JSON.parse(localStorage.getItem('path'));
+      }
+
+      all_path.forEach(function (item) {
+        ctx.beginPath();
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "red";
+
+        var _iterator2 = _createForOfIteratorHelper(item.path),
+            _step2;
+
+        try {
+          for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+            var _step2$value = _slicedToArray(_step2.value, 2),
+                x = _step2$value[0],
+                y = _step2$value[1];
+
+            ctx.lineTo(x, y);
+          }
+        } catch (err) {
+          _iterator2.e(err);
+        } finally {
+          _iterator2.f();
+        }
+
+        ctx.stroke();
+        ctx.closePath();
+      });
+    }
+
+    function click(event) {
+      var x = event.clientX;
+      var y = event.clientY;
+      renderPoints.push([x * 2, y * 2]);
+      render();
+    }
+
+    canvas.addEventListener('click', click);
+    var new_path = document.getElementById('new-path');
+    var finish_path = document.getElementById('finish-path');
+    var save_path = document.getElementById('save-path');
+    new_path.addEventListener('click', new_path_func);
+    finish_path.addEventListener('click', finish_path_func);
+    save_path.addEventListener('click', save_path_func);
+
+    function new_path_func(event) {
+      canvas.addEventListener('click', click);
+    }
+
+    function finish_path_func(event) {
+      var x = renderPoints[0][0];
+      var y = renderPoints[0][1];
+      renderPoints.push([x, y]);
+      render();
+      save_LC();
+      canvas.removeEventListener('click', click);
+      renderPoints = [];
+    }
+
+    function save_path_func(event) {
+      console.log('save');
+    }
+
+    function save_LC() {
+      var all_objects = [];
+
+      if (localStorage.getItem('path')) {
+        all_objects = JSON.parse(localStorage.getItem('path'));
+      }
+
+      var product = {
+        id: '1',
+        color: 'red',
+        path: renderPoints,
+        points: devider(renderPoints)
+      };
+      all_objects.push(product);
+      localStorage.removeItem('path'); //localStorage.setItem( 'path', JSON.stringify( all_objects ) )
+    }
+
+    function devider(rendPoints) {
+      var x = rendPoints.map(function (item) {
+        return item[0];
+      });
+      var minX = Math.min.apply(Math, _toConsumableArray(x));
+      var maxX = Math.max.apply(Math, _toConsumableArray(x));
+      var points_array = [];
+
+      var _loop = function _loop(_x) {
+        renderPoints.forEach(function (point, index) {
+          console.log(intersection(_x, [point, rendPoints[index + 1]])); // if( intersection( x, [point, rendPoints[index+1]] ) != [] ){
+          //     points_array.push( intersection( x, [point, rendPoints[index+1]] ) )
+          // }
+        });
+      };
+
+      for (var _x = minX; _x < maxX; _x++) {
+        _loop(_x);
+      }
+
+      return null;
+    }
+
+    function intersection(x, line) {
+      var k = (line[1][1] - line[0][1]) / (line[1][0] - line[0][0]);
+      var b = line[0][1] - k * line[0][0];
+
+      if (x >= line[0][0] && x <= line[1][0]) {
+        return [x, k * x + b];
+      } else {
+        return false;
+      }
+    }
+
+    render();
+  }
+
+  chart(document.getElementById('plugin-canvas'), []);
+});
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52574" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","assets/js/main.js"], null)
+//# sourceMappingURL=/main.cea5deef.js.map
